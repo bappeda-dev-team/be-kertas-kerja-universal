0dd4d9be3dfac041f12d84a0adc6521e154cd38c
agnarbriantama <agnarbriantama25@student.uns.ac.id>
2024-12-15 01:00:14 +0700

    filter user by kode opd

--- repository/users_repository.go
+++ repository/users_repository.go
@@ -9,7 +9,7 @@ import (
 type UserRepository interface {
 	Create(ctx context.Context, tx *sql.Tx, users domain.Users) (domain.Users, error)
 	Update(ctx context.Context, tx *sql.Tx, users domain.Users) (domain.Users, error)
-	FindAll(ctx context.Context, tx *sql.Tx) ([]domain.Users, error)
+	FindAll(ctx context.Context, tx *sql.Tx, kodeOpd string) ([]domain.Users, error)
 	FindById(ctx context.Context, tx *sql.Tx, id int) (domain.Users, error)
 	Delete(ctx context.Context, tx *sql.Tx, id int) error
 	FindByEmailOrNip(ctx context.Context, tx *sql.Tx, username string) (domain.Users, error)

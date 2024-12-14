78f138ba8d0848357a853265f6830698d1a4f609
agnarbriantama <agnarbriantama25@student.uns.ac.id>
2024-12-15 00:59:47 +0700

    filter user by kode opd

--- service/user_service.go
+++ service/user_service.go
@@ -9,7 +9,7 @@ type UserService interface {
 	Create(ctx context.Context, request user.UserCreateRequest) (user.UserResponse, error)
 	Update(ctx context.Context, request user.UserUpdateRequest) (user.UserResponse, error)
 	Delete(ctx context.Context, id int) error
-	FindAll(ctx context.Context) ([]user.UserResponse, error)
+	FindAll(ctx context.Context, kodeOpd string) ([]user.UserResponse, error)
 	FindById(ctx context.Context, id int) (user.UserResponse, error)
 	Login(ctx context.Context, request user.UserLoginRequest) (user.UserLoginResponse, error)
 }
